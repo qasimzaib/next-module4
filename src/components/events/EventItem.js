0c33501ae8ab7ddc172a78559e383f2ffdcbeda9
qasimzaib <zaib_qasim@hotmail.com>
2023-01-31 01:55:37 +0500

    Added button and styling for event list

--- src/components/events/EventItem.js
+++ src/components/events/EventItem.js
@@ -1,5 +1,8 @@
-import Link from "next/link";
 import styles from "./EventItem.module.css";
+import Button from "../ui/Button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem(props) {
 	const { title, image, date, location, id } = props.event;
@@ -19,14 +22,21 @@ export default function EventItem(props) {
 				<div className={styles.summary}>
 					<h2>{title}</h2>
 					<div className={styles.date}>
+						<DateIcon />
 						<time>{formattedDate}</time>
 					</div>
 					<div className={styles.address}>
+						<AddressIcon />
 						<address>{formattedAddress}</address>
 					</div>
 				</div>
 				<div className={styles.actions}>
-					<Link href={link}>Explore Event</Link>
+					<Button link={link}>
+						<span>Explore Event</span>
+						<span className={styles.icon}>
+							<ArrowRightIcon />
+						</span>
+					</Button>
 				</div>
 			</div>
 		</li>
