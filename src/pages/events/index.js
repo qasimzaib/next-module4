14f24029fccbabe47bb559c55b5426957f88d2b4
qasimzaib <zaib_qasim@hotmail.com>
2023-01-31 02:34:26 +0500

    All events page

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from "dummy-data";
+import EventList from "@/components/events/EventList";
+
 export default function EventsPage() {
+	const events = getAllEvents();
+
 	return (
 		<div>
-			<h1>Events Page</h1>
+			<EventList items={events} />
 		</div>
 	);
 }
