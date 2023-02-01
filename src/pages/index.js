6d00ad2eff6552fa829ee3a5c607e7c0d33d07f8
qasimzaib <zaib_qasim@hotmail.com>
2023-02-01 12:55:21 +0500

    Modify head tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import EventList from "@/components/events/EventList";
 import { getFeaturedEvents } from "@/helpers/api-utils";
+import Head from "next/head";
 
 export default function HomePage(props) {
 	return (
 		<div>
+			<Head>
+				<title>NextJS</title>
+				<meta name="description" content="NestJS Tutorial Application" />
+			</Head>
 			<EventList items={props.events} />
 		</div>
 	);
@@ -14,8 +19,8 @@ export async function getStaticProps() {
 
 	return {
 		props: {
-			events: featuredEvents, 
+			events: featuredEvents,
 		},
 		revalidate: 1800,
-	}
+	};
 }
